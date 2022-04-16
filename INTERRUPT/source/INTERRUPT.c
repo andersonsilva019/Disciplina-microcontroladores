92546f8c1dcd830f8f065708830f49d3d9001617
Anderson Silva <andersonnsilva@alu.ufc.br>
2022-04-16 14:57:43 -0300

    initial commit 2

--- INTERRUPT/source/INTERRUPT.c
+++ INTERRUPT/source/INTERRUPT.c
@@ -29,7 +29,7 @@
  */
 
 /**
- * @file    interrupt.c
+ * @file    INTERRUPT.c
  * @brief   Application entry point.
  */
 #include <stdio.h>
@@ -57,16 +57,6 @@ int main(void) {
     BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World\n");
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
     return 0 ;
 }
